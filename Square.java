27f04efa0f0b5f6957af113f0840493cf675b326
Tayyaba Bhatti <134331306+TaiyabaBhatti@users.noreply.github.com>
2023-06-08 12:13:26 +0500

    Pattern Question

---
+++ Square.java
@@ -0,0 +1,19 @@
+package com.tayyaba;
+import java.util.Scanner;
+public class Square {
+    public static void main(String[] args) {
+
+        for (int row=1;row<=5;row++){
+            for (int col=1;col<=5;col++){
+                System.out.print("* ");
+
+
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
