82318acfb9f4aa168109175d7eadef55a4b8da92
Tayyaba Bhatti <134331306+TaiyabaBhatti@users.noreply.github.com>
2023-06-08 13:29:47 +0500

    Loops and Conditionals

---
+++ CommissionPercentage.java
@@ -0,0 +1,18 @@
+package com.Conditionals;
+
+public class CommissionPercentage {
+    public static void main(String[] args) {
+        // Commision percentage
+
+        int Camount=550;
+        int saleamount=1500;
+        System.out.println("Commision amount for Basket ball:"+Camount);
+        System.out.println("Actual saleprice for basket ball:"+saleamount);
+        float Cper=(float) Camount*100/saleamount;
+        System.out.println("Commission percentage is :"+Cper);
+
+
+
+
+    }
+}
